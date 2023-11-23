f443e71b963e378a8f67d8ef72f2ac142f1ce613
HairuMossa <117511087+HairuMossa@users.noreply.github.com>
2023-11-23 11:29:46 +0300

    physical pins identified
    SPI pins are identified and listed

--- LIS3DSHTR Accelerometer using STM32F407VG/src/main.c
+++ LIS3DSHTR Accelerometer using STM32F407VG/src/main.c
@@ -1,11 +1,14 @@
 #include "stm32f407xx.h"
 
+// SPI communication is established between the MCU and the Accelerometer
 // from the STM32F407VG discovery datasheet the SDA and SCL connections are as follows
-// SDA - PA7
-// SCL - PA5
+// SPI1_SCK   - PA5
+// SPI1_MISO  - PA6
+// SPI1_MOSI  - PA7
+ 
 
 
-#define WHO_AM_I       00001111
+#define WHO_AM_I       00001111 // device slave address
 #define OUT_T          00001100 // temperature output
 
 
